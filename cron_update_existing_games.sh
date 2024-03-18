43a90984bc51ff0bd50b7005fa6c3e872b306cff
Russell Martin <russell@martinandbrown.com>
2024-03-18 11:36:22 -0400

    Add cron job to update game listings on a regular basis

---
+++ cron_update_existing_games.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+cd /Users/martin/Dropbox/Python/BoardGame
+/Users/martin/.local/bin/poetry run python update_existing_games.py
---
+++ update_existing_games.py
@@ -0,0 +1,68 @@
+'''A script to update the existing game information.  Takes a block of indices
+and attempts to scrape the game information from BGG.  Then combines this
+new information with other info for games that haven't been updated, and
+writes out the data to the same dill file.
+'''
+import time
+import dill
+import glob
+import re
+
+import pandas as pd
+import numpy as np 
+
+from datetime import datetime
+
+from constants import GAME_DATA
+from api_functions import getGame
+
+window = 10000
+
+if __name__ == '__main__':
+    game_file = sorted(glob.glob(f'{GAME_DATA}/all-to-*.dill'), key=lambda x: int(re.search(r'[\d]+',x).group(0)))[-1]
+
+    with open(game_file, 'rb') as f:
+        all_games = dill.load(f)
+    
+    with open('update_existing_games_start.txt', 'r') as f:
+        first = int(f.readline())
+    
+    to_update = all_games.index[first:first+window].tolist()
+    
+    step_size = 100
+    result = []
+    start_time = datetime.now()
+    print('---------------------------')
+    print(f'Start time: {start_time}')
+    print(f'{len(all_games)} games in the starting collection.')
+    
+    for index in range(0, len(to_update), step_size):
+        games = getGame(to_update[index:index+step_size])
+        if games is not None:
+            result.append(games)
+        time.sleep(4)
+    
+    end_time = datetime.now()
+    print(f'End time: {end_time}')
+    
+    #  Note that we may not quite capture all the data for game indices that were in the original 
+    #  all_games DataFrame, hence this is why we are taking the difference between the 
+    #  set of indices in the "result" and the portion of "all_games" that we are trying to update.  
+    #  We don't want to lose those games for which we might not have gotten a valid result in 
+    #  the update portion of this script.  
+    if result:
+        result = pd.concat(result)
+        print(f'Updated {len(result)} games in {end_time - start_time}.')
+        new = pd.concat([all_games.loc[list(set(to_update).difference(result.index))], 
+                         all_games.iloc[:first], all_games.iloc[first+window:], 
+                         result]).sort_index()
+    
+        print(f'{len(new)} games in the updated collection.')
+        with open(game_file, 'wb') as f:
+            dill.dump(new, f)
+        
+        first += window 
+        if first > len(new):
+            first = 0
+        with open('update_existing_games_start.txt', 'w') as f:
+            f.write(str(first))
