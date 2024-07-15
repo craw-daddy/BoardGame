fbb27a50c886b4257e74e0e6c02c208414dc914a
Russell Martin <russell@martinandbrown.com>
2024-07-15 17:45:39 -0400

    Update the script for updating existing game information due to API changes

--- update_existing_games.py
+++ update_existing_games.py
@@ -16,7 +16,8 @@ from datetime import datetime
 from constants import GAME_DATA
 from api_functions import getGame
 
-window = 10000
+WINDOW = 10000
+STEP_SIZE = 100
 
 if __name__ == '__main__':
     game_file = sorted(glob.glob(f'{GAME_DATA}/all-to-*.dill'), key=lambda x: int(re.search(r'[\d]+',x).group(0)))[-1]
@@ -27,17 +28,16 @@ if __name__ == '__main__':
     with open('update_existing_games_start.txt', 'r') as f:
         first = int(f.readline())
     
-    to_update = all_games.index[first:first+window].tolist()
+    to_update = all_games.index[first:first+WINDOW].tolist()
     
-    step_size = 100
     result = []
     start_time = datetime.now()
     print('---------------------------')
     print(f'Start time: {start_time}')
     print(f'{len(all_games)} games in the starting collection.')
     
-    for index in range(0, len(to_update), step_size):
-        games = getGame(to_update[index:index+step_size])
+    for index in range(0, len(to_update), STEP_SIZE):
+        games = getGame(to_update[index:index+STEP_SIZE])
         if games is not None:
             result.append(games)
         time.sleep(4)
@@ -54,14 +54,14 @@ if __name__ == '__main__':
         result = pd.concat(result)
         print(f'Updated {len(result)} games in {end_time - start_time}.')
         new = pd.concat([all_games.loc[list(set(to_update).difference(result.index))], 
-                         all_games.iloc[:first], all_games.iloc[first+window:], 
+                         all_games.iloc[:first], all_games.iloc[first+WINDOW:], 
                          result]).sort_index()
     
         print(f'{len(new)} games in the updated collection.')
         with open(game_file, 'wb') as f:
             dill.dump(new, f)
         
-        first += window 
+        first += WINDOW
         if first > len(new):
             first = 0
         with open('update_existing_games_start.txt', 'w') as f:
