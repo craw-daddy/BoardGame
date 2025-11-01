c3f1f032b56cc48ff5bec0385f7d39ea22360d02
Russell Martin <russell@martinandbrown.com>
2025-11-01 13:19:22 -0400

    Add comment to constants.py about BGG authorization token

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-BASE_API = 'https://www.boardgamegeek.com/xmlapi2'
+BASE_API = 'https://boardgamegeek.com/xmlapi2'
 
 USER_DATA = 'USERS'
 GEEKBUDDIES_DATA = 'GEEKBUDDIES'
@@ -7,8 +7,13 @@ EXTRA_DATA = 'EXTRA_DATA'
 
 SLEEP_DELAY = 12 
 
+#  Set up the "Authorization dictionary" in order to pass credentials
+#  into BGG, as required.  
+#  See https://boardgamegeek.com/wiki/page/XML_API_Terms_of_Use#
+#  Import this dictionary as needed, in order to pass the authorization token
+#  in using the "requests" package.  
 with open('TOKENS/bgg-token.txt', 'r') as f:
     AUTHORIZATION_TOKEN = f.readline().strip()
 
-AUTHORIZATION_DICT = {'Authorization:' :f'Bearer {AUTHORIZATION_TOKEN}'}
+AUTHORIZATION_DICT = {'Authorization' :f'Bearer {AUTHORIZATION_TOKEN}'}
 
